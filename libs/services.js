a75c09f59135f04c78074ad4dc1a407cbb4f893c
席铭 <ximing@in-deco.com>
2021-04-27 14:02:20 +0800

    因为接口已经增加了假数据，localhost也可以获取devgw的数据，所以删除header中origin的设置。

--- libs/services.js
+++ libs/services.js
@@ -1,21 +1,14 @@
 import axios from 'axios'
 
 let apiDomain = ''
-let headers = {}
 
 if (process.env.NODE_ENV === 'development') {
   apiDomain = process.env.NEXT_PUBLIC_CUSTOM_DOMAIN
-  headers = { origin: apiDomain }
 }
 
 export const Services = {
-  findAllChannels: () => axios.get(apiDomain + '/api/v1/wechat/menu/preview/list', { headers }),
-  findAllFooters: () =>
-    axios.get(apiDomain + '/api/v1/wechat/website/companyinfo/pc/view', { headers }),
+  findAllChannels: () => axios.get(apiDomain + '/api/v1/wechat/menu/preview/list'),
+  findAllFooters: () => axios.get(apiDomain + '/api/v1/wechat/website/companyinfo/pc/view'),
   findAllData: params =>
-    axios.post(
-      apiDomain + '/wechatapi/api/v1/wechat/homePage/pc/getHomePagePublishedData',
-      params,
-      { headers },
-    ),
+    axios.post(apiDomain + '/wechatapi/api/v1/wechat/homePage/pc/getHomePagePublishedData', params),
 }
