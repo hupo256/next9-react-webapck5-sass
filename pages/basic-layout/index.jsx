db1a75b762ab29d6e0d8f830ecedc117db6795f8
席铭 <ximing@in-deco.com>
2021-05-10 13:18:45 +0800

    添加首尾框架

--- pages/basic-layout/index.jsx
+++ pages/basic-layout/index.jsx
@@ -1,7 +1,7 @@
-import BasicLayout from '@components/BasicLayout/BasicLayout'
+import BasicLayout from '@components/Home/HomePageLayout'
 
 const BasicLayoutDemoPage = () => {
-  return <BasicLayout>TEST</BasicLayout>
+  return <BasicLayout title={'whatever you want'}>TEST</BasicLayout>
 }
 
 export default BasicLayoutDemoPage
