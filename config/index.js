eb051e519676accb33cff5ee28504b48b620ab59
席铭 <ximing@in-deco.com>
2021-05-12 16:09:44 +0800

    Must REVERT it once DONGDONG finished modification

--- config/index.js
+++ config/index.js
@@ -1,10 +1,13 @@
 // 接口host
 const { NODE_ENV } = process.env
 
-let prefix = ''
-const isProd = NODE_ENV === 'production'
-NODE_ENV === 'development' && (prefix = 'dev')
-NODE_ENV === 'test' && (prefix = 'test')
-const host = isProd ? '//gateway.ingongdi.com/' : `//${prefix}gw.ingongdi.com/`
+// todo... 暂时版， 提测前 请 修改为真实逻辑
+// let prefix = ''
+// const isProd = NODE_ENV === 'production'
+// NODE_ENV === 'development' && (prefix = 'dev')
+// NODE_ENV === 'test' && (prefix = 'test')
+// const host = isProd ? '//gateway.ingongdi.com/' : `//${prefix}gw.ingongdi.com/`
+
+const host = 'http://devgw.ingongdi.com/'
 
 export default { host }
--- pages/basic-layout/index.jsx
+++
@@ -1,7 +0,0 @@
-import BasicLayout from '@components/PcPreview/HomePageLayout'
-
-const BasicLayoutDemoPage = () => {
-  return <BasicLayout title={'whatever you want'}>TEST</BasicLayout>
-}
-
-export default BasicLayoutDemoPage
