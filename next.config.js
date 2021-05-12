abe2840739ad714835fdb9c2e63d1f06ca7fadcb
hupo256 <hupo256@qq.com>
2021-05-12 19:05:44 +0800

    update webpack

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 const path = require('path')
-
 module.exports = {
   pageExtensions: ['mdx', 'jsx', 'js', 'scss', 'less'],
   sassOptions: {
@@ -15,3 +14,20 @@ module.exports = {
     ],
   },
 }
+
+// const withCSS = require('@zeit/next-css')
+// const withSass = require('@zeit/next-sass')
+
+// module.exports = () => withSass(withCSS())
+
+// module.exports = {
+//   webpack: (config, options) => {
+//     config.module.rules.push({
+//       test: /\.s(a|c)ss$/,
+//       use: ['css-loader', 'sass-loader'],
+//       exclude: /node_modules/,
+//     })
+
+//     return config
+//   },
+// }
