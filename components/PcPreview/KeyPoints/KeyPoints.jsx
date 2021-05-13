75a1a9659ba44d198042717888dbbcbc35ec9b7b
席铭 <ximing@in-deco.com>
2021-05-13 16:40:32 +0800

    首页 点击 亮点 跳转

--- components/PcPreview/KeyPoints/KeyPoints.jsx
+++ components/PcPreview/KeyPoints/KeyPoints.jsx
@@ -1,11 +1,20 @@
 import styles from './KeyPoints.module.scss'
 import _ from 'lodash'
 
+import { typeMap, paramMap } from '@libs/constants.js'
+
 const KeyPoints = ({ pointsList }) => {
   return (
     <div className={styles.featurePoints}>
       {_.map(pointsList, (feature, index) => (
-        <div key={index} className={styles.featurePoint}>
+        <div
+          key={index}
+          className={styles.featurePoint}
+          onClick={() =>
+            (window.location.href = `/${typeMap[feature.type]}/details?${paramMap[feature.type]}=${feature.uid}`)
+          }
+          style={{ cursor: 'pointer' }}
+        >
           <img src={feature.icon} />
           <p className={styles.pointTitle}>{feature.title}</p>
           <p className={styles.pointSubTitle}>{feature.desc}</p>
---
+++ libs/constants.js
@@ -0,0 +1,15 @@
+export const typeMap = {
+  case: 'cases',
+  site: 'sites',
+  article: 'articles',
+  design: 'designers',
+  special: 'special',
+  games: 'games',
+}
+
+export const paramMap = {
+  case: 'uid',
+  site: 'gongdiUid',
+  article: 'articleUid',
+  design: 'uid',
+}
