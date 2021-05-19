836ca20851d1389a1bb543e9725075a60cb4cea6
hupo256 <hupo256@qq.com>
2021-05-19 18:23:43 +0800

    add 拾 to CHN_NUM_CHAR

--- components/roomType/index.jsx
+++ components/roomType/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const CHN_NUM_CHAR = ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九']
+const CHN_NUM_CHAR = ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九', '拾']
 
 export default function RoomType(props) {
   const { bedroom, liveroom } = props
