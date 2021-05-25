ea4c0a8ab16a8630fb4c36e3e630cd08b09fce46
hupo256 <hupo256@qq.com>
2021-05-25 22:00:50 +0800

    add share with error

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   pageExtensions: ['jsx', 'js'],
-  trailingSlash: true,
+  // trailingSlash: true,
   sassOptions: { includePaths: [path.join(__dirname, 'styles')] },
   future: { webpack5: true },
   webpack: (config, options) => {
