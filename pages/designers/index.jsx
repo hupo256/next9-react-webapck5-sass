e6c0b69cdf13042da3715f6e3c9e93f4b282aa8f
hupo256 <hupo256@qq.com>
2021-06-16 00:38:01 +0800

    fix headConfigx to headConfig

--- pages/designers/index.jsx
+++ pages/designers/index.jsx
@@ -39,7 +39,7 @@ export default function Site(props) {
   }
 
   return (
-    <BasicLayout headConfigx={{ title: '找设计师' }} pushType="designer">
+    <BasicLayout headConfig={{ title: '找设计师' }} pushType="designer">
       <div className="grayBg">
         <div className="conBox">
           {/* breadBar */}
